4ade08852006ae91d9f12685b1c189fe009908de
chris <orthagonal1@gmail.com>
2017-01-25 23:59:19 -0600

    blocking conditions

--- index.js
+++ index.js
@@ -1,50 +1,79 @@
 'use strict';
 const async = require('async');
 
+
 module.exports = (tape, events) => {
   let beforeCalled = false;
   let beforeResult = undefined;
   let outstandingTestCounter = 0;
+  let callIsOutstanding = false;
   const callTest = (testDescription, testMethod) => {
     outstandingTestCounter++;
-    async.autoInject({
-      // handle before:
-      before(done) {
-        if (!events.before || beforeCalled) {
-          return done(null, beforeResult);
-        }
-        return events.before((err, result) => {
-          if (err) {
-            return done(err);
+    async.whilst(() => callIsOutstanding, done => setTimeout(done, 25), () => {
+      callIsOutstanding = true;
+      async.autoInject({
+        // handle before:
+        before(done) {
+          if (!events.before || beforeCalled) {
+            return done(null, beforeResult);
           }
-          beforeCalled = true;
-          beforeResult = result;
-          done(null, beforeResult);
-        });
-      },
-      // handle beforeEach:
-      beforeEach(before, done) {
-        if (!events.beforeEach) {
+          return events.before((err, result) => {
+            if (err) {
+              return done(err);
+            }
+            beforeCalled = true;
+            beforeResult = result;
+            done(null, beforeResult);
+          });
+        },
+        // handle beforeEach:
+        beforeEach(before, done) {
+          if (!events.beforeEach) {
+            return done();
+          }
+          const args = [];
+          if (events.before) {
+            args.push(before);
+          }
+          args.push(done);
+          events.beforeEach.apply(this, args);
+        },
+        // set up event listeners to handle after and afterEach:
+        registerAfter(before, beforeEach, done) {
+          if (!events.after) {
+            return done();
+          }
+          // set up 'after' event:
+          tape.onFinish(() => {
+            // 'after' only fires after the last outstanding test is deducted:
+            outstandingTestCounter--;
+            if (outstandingTestCounter === 0) {
+              // set up args and call it:
+              const args = [];
+              if (events.before) {
+                args.push(before);
+              }
+              if (events.beforeEach) {
+                args.push(beforeEach);
+              }
+              // todo: this needs to be something else:
+              args.push((err, results) => {
+                if (err) {
+                  throw err;
+                }
+              });
+              events.after.apply(this, args);
+            }
+          });
           return done();
-        }
-        const args = [];
-        if (events.before) {
-          args.push(before);
-        }
-        args.push(done);
-        events.beforeEach.apply(this, args);
-      },
-      // set up event listeners to handle after and afterEach:
-      registerAfter(before, beforeEach, done) {
-        if (!events.after) {
-          return done();
-        }
-        // set up 'after' event:
-        tape.onFinish(() => {
-          // 'after' only fires after the last outstanding test is deducted:
-          outstandingTestCounter--;
-          if (outstandingTestCounter === 0) {
-            // set up args and call it:
+        },
+        registerAfterEach(before, beforeEach, done) {
+          const t = tape(testDescription);
+          if (!events.afterEach) {
+            callIsOutstanding = false;
+            return done(null, t);
+          }
+          t.on('end', () => {
             const args = [];
             if (events.before) {
               args.push(before);
@@ -52,54 +81,32 @@ module.exports = (tape, events) => {
             if (events.beforeEach) {
               args.push(beforeEach);
             }
-            args.push((err, results) => {
+            args.push((err) => {
               if (err) {
                 throw err;
               }
+              callIsOutstanding = false;
             });
-            events.after.apply(this, args);
-          }
-        });
-        return done();
-      },
-      registerAfterEach(before, beforeEach, done) {
-        const t = tape(testDescription);
-        // set up 'afterEach' event:
-        if (!events.afterEach) {
-          return done(null, t);
-        }
-        t.on('end', () => {
-          const args = [];
+            events.afterEach.apply(this, args);
+          });
+          done(null, t);
+        },
+        // perform the actual test:
+        mainTest(registerAfter, registerAfterEach, before, beforeEach, done) {
+          const args = [registerAfterEach];
           if (events.before) {
             args.push(before);
           }
           if (events.beforeEach) {
             args.push(beforeEach);
           }
-          args.push((err) => {
-            if (err) {
-              throw err;
-            }
-          });
-          events.afterEach.apply(this, args);
-        });
-        done(null, t);
-      },
-      // perform the actual test:
-      mainTest(registerAfter, registerAfterEach, before, beforeEach, done) {
-        const args = [registerAfterEach];
-        if (events.before) {
-          args.push(before);
+          return done(null, testMethod.apply(this, args));
+        },
+      }, (err) => {
+        if (err) {
+          throw err;
         }
-        if (events.beforeEach) {
-          args.push(beforeEach);
-        }
-        return done(null, testMethod.apply(this, args));
-      },
-    }, (err) => {
-      if (err) {
-        throw err;
-      }
+      });
     });
   };
   return callTest;
